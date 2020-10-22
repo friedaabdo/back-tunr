b8d4de4ad0ec8196c6d69efcbe853651b4075c13
SeanUnland <69156579+SeanUnland@users.noreply.github.com>
2020-10-22 12:05:01 -0400

    completed model

---
+++ models/song.js
@@ -0,0 +1,13 @@
+const mongoose = require("../db/connection");
+
+const Schema = mongoose.Schema;
+
+const songSchema = new Schema({
+  title: String,
+  artist: String,
+  time: Number,
+});
+
+const Song = mongoose.model("Song", songSchema);
+
+module.exports = Cat;
